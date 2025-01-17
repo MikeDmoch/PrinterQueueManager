b851ab26ddea190d32055c8c74177cc702a755a2
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-17 20:58:30 +0100

    Add project files.

---
+++ PrinterQueueManager/Printer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterQueueManager
+{
+    internal class Printer
+    {
+        void Print(string document) => Console.WriteLine($"Drukowanie: {document}");
+        void CancelPrint(string document) => Console.WriteLine($"Zatrzymano drukowanie: {document}");
+
+    }
+}
